6718c96983de35e38d69107c3e197e02286ea6ba
Pawan <ps5042189@gmail.com>
2024-12-26 09:55:06 +0530

    Initial commit with README

---
+++ src/main/java/in/KataLmsApplication.java
@@ -0,0 +1,13 @@
+package in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataLmsApplication.class, args);
+	}
+
+}
